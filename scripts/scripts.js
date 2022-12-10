757a57c6ba5de5b5c6ceea5d92677b8a3c5da2b6
ZManak <javifuer@gmail.com>
2022-12-10 22:24:31 +0100

    updated/writeCards()

---
+++ scripts/scripts.js
@@ -0,0 +1,104 @@
+const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "imageLink": "assets/things-fall-apart.jpg",
+        "language": "English",
+        "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Hans Christian Andersen",
+        "country": "Denmark",
+        "imageLink": "assets/fairy-tales.jpg",
+        "language": "Danish",
+        "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+        "pages": 784,
+        "title": "Fairy tales",
+        "year": 1836
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "imageLink": "assets/the-divine-comedy.jpg",
+        "language": "Italian",
+        "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+    {
+        "author": "Unknown",
+        "country": "Sumer and Akkadian Empire",
+        "imageLink": "assets/the-epic-of-gilgamesh.jpg",
+        "language": "Akkadian",
+        "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+        "pages": 160,
+        "title": "The Epic Of Gilgamesh",
+        "year": -1700
+    },
+    {
+        "author": "Unknown",
+        "country": "Achaemenid Empire",
+        "imageLink": "assets/the-book-of-job.jpg",
+        "language": "Hebrew",
+        "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+        "pages": 176,
+        "title": "The Book Of Job",
+        "year": -600
+    }]
+
+
+function writeCards() {
+
+    const principal = document.createElement("main");
+    const seccion = document.createElement("section");
+    principal.appendChild(seccion);
+
+    for (let i = 0; i < books.length; i++) {
+
+        const art = document.createElement("article");
+        let encabezado = document.createElement("h3");
+        encabezado.innerHTML = "Author: " + books[i].author;
+        art.appendChild(encabezado);
+        let pais = document.createElement("p");
+        pais.innerHTML = "Country: " + books[i].country;
+        art.appendChild(pais);
+        let imagen = document.createElement("img");
+        imagen.src = books[i].imageLink;
+        art.appendChild(imagen)
+        let idioma = document.createElement("p");
+        idioma.innerHTML = "Language: " + books[i].language;
+        art.appendChild(idioma);
+        let enlace = document.createElement("a");
+        let textoEnlace = document.createTextNode(books[i].link);
+        enlace.appendChild(textoEnlace);
+        enlace.title = books[i].link;
+        enlace.href = books[i].link;
+        art.appendChild(enlace);
+        let paginas = document.createElement("p");
+        paginas.innerHTML = "Pages: " + books[i].pages;
+        art.appendChild(paginas);
+        let titulo = document.createElement("p");
+        titulo.innerHTML = "Title: " + books[i].title;
+        art.appendChild(titulo);
+        let año = document.createElement("p");
+        año.innerHTML = "Year: " + books[i].year;
+        art.appendChild(año);
+
+        seccion.appendChild(art);
+
+    }
+
+    const cuerpo = document.querySelector("body");
+    cuerpo.appendChild(principal);
+    //const faldon = document.querySelector("footer");
+    //principal.insertBefore(principal, faldon);
+
+}
+
+
+
+
